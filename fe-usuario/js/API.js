1552e096625372e146d9d97e230b110eaa64193a
williams rubio <williamsrubio10@gmail.com>
2026-09-21 10:44:15 -0600

    Actualizar API y frontend

--- fe-usuario/js/API.js
+++ fe-usuario/js/API.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:5000/usuario";
+const url = "https://apiejemplowilliamsrubio-production.up.railway.app/usuario";
 
 export const nuevoCliente = async cliente => {
     try {
